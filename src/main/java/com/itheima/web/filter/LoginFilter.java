a40892e4ef9661423497b141dba9e0523243c990
aa <2419805602@qq.com>
2023-03-29 23:31:20 +0800

    home win8 ideaIU

--- src/main/java/com/itheima/web/filter/LoginFilter.java
+++ src/main/java/com/itheima/web/filter/LoginFilter.java
@@ -1,4 +1,22 @@
 package com.itheima.web.filter;
 
-public class LoginFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        chain.doFilter(request, response);
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
 }
