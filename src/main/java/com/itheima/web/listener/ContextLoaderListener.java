7fcf1570452d9840dec7d21b43c19b962ec1ef5f
hyb2419805602 <2419805602@qq.com>
2023-03-30 21:52:16 +0800

    home ubuntu16 ideaIC

--- src/main/java/com/itheima/web/listener/ContextLoaderListener.java
+++ src/main/java/com/itheima/web/listener/ContextLoaderListener.java
@@ -9,7 +9,7 @@ public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         // 加载资源
-
+        System.out.println("ContextLoaderListener...");
     }
 
     @Override
